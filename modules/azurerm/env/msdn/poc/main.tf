9c1ef4e7fc9c1d92fa05b035ac585f11be576619
Corey Vittitow <corey.vittitow@e-farmcredit.com>
2021-12-08 14:39:37 -0500

    Fix mismatched variable reference on tenant_id

--- modules/azurerm/env/msdn/poc/main.tf
+++ modules/azurerm/env/msdn/poc/main.tf
@@ -27,5 +27,5 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  tenant_id       = var.subscription_id
+  tenant_id       = var.tenant_id
 }
